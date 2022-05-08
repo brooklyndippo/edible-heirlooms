7dfed1cbf92045e44376637501950907da4a69b2
Brooklyn Dippo <brooklyndippo@gmail.com>
2022-05-07 19:26:41 -1000

    created db models for User, Recipe, Collection

--- recipe_app/models.py
+++ recipe_app/models.py
@@ -1,1 +1,62 @@
-# Create your models here.
+from sqlalchemy import table
+from sqlalchemy_utils import URLType
+from flask_login import UserMixin
+from recipe_app.extensions import db
+#from recipe_app.utils import FormEnum
+
+class RecipeCategory(FormEnum):
+    """Categories of grocery items."""
+    BREAKFAST = 'Breakfast'
+    LUNCH = 'Lunch'
+    DINNER = 'Dinner'
+    DESSERT = 'Dessert'
+    HOLIDAY = 'Holiday'
+    OTHER = 'Other'
+
+
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    origin_story = db.Column(db.String(1000), nullable=False)
+    category = db.Column(db.Enum(RecipeCategory), default=RecipeCategory.OTHER)
+    serving_size = db.Column(db.String(200), nullable=False)
+    ingredients = db.Column(db.String(500), nullable=False)
+    preparation = db.Column(db.String(3000), nullable=False)
+    collections = db.relationship('Collection', secondary='recipe_collection', back_populates='recipes')
+    shared_by = db.Column(db.Integer, db.ForeignKey('user.id'))
+    sharer = db.relationship('User')
+
+
+class Collection(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(500), nullable=False)
+    recipes = db.relationship('Recipes', secondary='recipe_collection', back_populates='collections')
+    curated_by = db.Column(db.Integer, db.ForeignKey('user.id'))
+    curator = db.relationship('User')
+    
+    
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column (db.String(20), nullable=False, unique=True)
+    password = db.Column (db.String(20), nullable=False)
+    first_name = db.Column (db.String(20), nullable=False)
+    last_name = db.Column (db.String(20), nullable=False)
+    recipes = db.relationship('recipe', secondary='user_recipe', back_populated='curated_by') 
+    collections = db.relationship('collection', secondary='user_collection', back_populates='author')
+
+# user_recipes = db.Table('user_recipe',
+#     db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+#     db.Column('recipe_id', db.Integer, db.ForeignKey('recipe.id'))
+# )
+
+# user_collections = db.Table('user_collection',
+#     db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+#     db.Column('collection_id', db.Integer, db.ForeignKey('collection.id'))
+# )
+
+recipe_collections = db.Table('recipe_collection',
+    db.Column('recipe_id', db.Integer, db.ForeignKey('recipe.id')),
+    db.Column('collection_id', db.Integer, db.ForeignKey('collection.id'))
+)
