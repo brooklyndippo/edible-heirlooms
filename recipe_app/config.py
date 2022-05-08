f1a658ed15012a4d86a78f5ac54bc5ebfaa21ddd
Brooklyn Dippo <brooklyndippo@gmail.com>
2022-05-07 23:10:12 -1000

    updated db url

--- recipe_app/config.py
+++ recipe_app/config.py
@@ -7,6 +7,8 @@ load_dotenv()
 class Config(object):
     """Set environment variables."""
 
+    #Run Locally
+    # SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SECRET_KEY = os.getenv('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
