bf3e2811d0b2b50eadaa25614c2dc4a4afd2cf07
Brooklyn Dippo <brooklyndippo@gmail.com>
2022-05-11 09:50:56 -1000

    increase password length

--- recipe_app/models.py
+++ recipe_app/models.py
@@ -45,7 +45,7 @@ class Collection(db.Model):
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column (db.String(20), nullable=False, unique=True)
-    password = db.Column (db.String(20), nullable=False)
+    password = db.Column (db.String(200), nullable=False)
     first_name = db.Column (db.String(20), nullable=False)
     last_name = db.Column (db.String(20), nullable=False)
 
